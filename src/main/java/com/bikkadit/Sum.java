526bebfce70316bae90b3c0534db73106d211662
mayuriPatil <mayuripatil0363@gmail.com>
2024-09-26 12:20:43 +0530

    comit

---
+++ src/main/java/com/bikkadit/Sum.java
@@ -0,0 +1,12 @@
+package com.bikkadit;
+
+public class Sum {
+	
+	public static void main(String[] args) {
+		
+		SumAndSub sm = new SumAndSub();
+		int i =sm.sum(12, 10);
+		System.out.println(i);
+	}
+
+}
